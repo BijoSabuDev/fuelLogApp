1c633c548bf4cf7bc894901054f93fd9f459143d
BijoSabuDev <bijo@cordovacloud.com>
2024-01-15 17:47:15 +0530

    vehiclescreenchanges

--- fuellog/lib/view/VehicleScreen/increment_decrement_buttons.dart
+++ fuellog/lib/view/VehicleScreen/increment_decrement_buttons.dart
@@ -1,10 +1,13 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class IncrementDecrementButtons extends StatefulWidget {
-  final double initValue;
-  const IncrementDecrementButtons({
+  double initValue;
+
+  IncrementDecrementButtons({
     super.key,
     required this.scrollController,
     required this.initValue,
@@ -18,21 +21,34 @@ class IncrementDecrementButtons extends StatefulWidget {
 }
 
 class _IncrementDecrementButtonsState extends State<IncrementDecrementButtons> {
+  late Timer _timer;
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         GestureDetector(
-          onLongPress: () {
-            widget.initValue - 5;
+          onLongPressStart: (details) {
+            _timer = Timer.periodic(const Duration(milliseconds: 200), (timer) {
+              setState(() {
+                widget.initValue -= 5;
+                widget.scrollController.animateToItem(
+                  widget.scrollController.selectedItem - 5,
+                  duration: const Duration(milliseconds: 200),
+                  curve: Curves.easeInOut,
+                );
+              });
+            });
+          },
+          onLongPressEnd: (details) {
+            _timer.cancel();
             widget.scrollController.animateToItem(
-              widget.scrollController.selectedItem - 5,
+              widget.scrollController.selectedItem,
               duration: const Duration(milliseconds: 200),
               curve: Curves.easeInOut,
             );
           },
           onTap: () {
-            widget.initValue - 1;
+            widget.initValue -= 1;
             widget.scrollController.animateToItem(
               widget.scrollController.selectedItem - 1,
               duration: const Duration(milliseconds: 300),
@@ -61,16 +77,28 @@ class _IncrementDecrementButtonsState extends State<IncrementDecrementButtons> {
           height: 2.h,
         ),
         GestureDetector(
-          onLongPress: () {
-            widget.initValue + 5;
+          onLongPressStart: (details) {
+            _timer = Timer.periodic(const Duration(milliseconds: 200), (timer) {
+              setState(() {
+                widget.initValue += 5;
+                widget.scrollController.animateToItem(
+                  widget.scrollController.selectedItem + 5,
+                  duration: const Duration(milliseconds: 200),
+                  curve: Curves.easeInOut,
+                );
+              });
+            });
+          },
+          onLongPressEnd: (details) {
+            _timer.cancel();
             widget.scrollController.animateToItem(
-              widget.scrollController.selectedItem + 5,
+              widget.scrollController.selectedItem,
               duration: const Duration(milliseconds: 200),
-              curve: Curves.ease,
+              curve: Curves.easeInOut,
             );
           },
           onTap: () {
-            widget.initValue + 1;
+            widget.initValue += 1;
             widget.scrollController.animateToItem(
               widget.scrollController.selectedItem + 1,
               duration: const Duration(milliseconds: 300),
